1b32127d2eb236ce527a7e94bbad9eebfbd4d02a
Oleg Kuzmin <oleg.kuzmin@arcadia.spb.ru>
2020-01-29 11:46:39 +0300

    added all the prettier config options

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,27 @@
 'use strict';
 
+// @see: https://prettier.io/docs/en/options.html
+
+// Commented options below have default values
+
 module.exports = {
-    semi: true,
-    trailingComma: 'all',
-    singleQuote: true,
     printWidth: 120,
     tabWidth: 4,
+    // useTabs: false,
+    semi: true,
+    singleQuote: true,
+    // quoteProps: 'as-needed',
+    // jsxSingleQuote: false,
+    trailingComma: 'all',
+    // bracketSpacing: true,
+    // jsxBracketSameLine: false,
+    // arrowParens: 'avoid',
+    // rangeStart: 0,
+    // rangeEnd: Infinity,
+    // requirePragma: false,
+    // insertPragma: false,
+    proseWrap: 'always',
+    // htmlWhitespaceSensitivity: 'css',
+    // vueIndentScriptAndStyle: false,
     endOfLine: 'lf',
 };
